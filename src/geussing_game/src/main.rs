dbc5aee5d876c72d6499352ce7857bd0847a43c8
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-04-27 23:13:43 +0800

    None

--- src/geussing_game/src/main.rs
+++ src/geussing_game/src/main.rs
@@ -1,12 +1,31 @@
+//prelude是每个rust程序的默认导入模块
+use rand::Rng; // trait
+use std::cmp::Ordering;
 use std::io;
-use std::prelude::*; //prelude是每个rust程序的默认导入模块
 fn main() {
     println!("猜数！");
     println!("猜测一个数");
+    let secret_number = rand::thread_rng().gen_range(1..101); //左闭右开
+    println!("神秘数字:{}", secret_number);
     /* let foo =1;
     let bar =foo;
     foo=2; 没有mut关键字修饰变量，所以foo变量 immutable */
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取");
-    println!("你猜测的数是:{}", guess)
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取");
+        let guess: u32 = match guess.trim().parse() {
+            // expect("Please type a number!");
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("你猜测的数是:{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
+    }
 }
